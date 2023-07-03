f69557812e8dd388b0642754706efc78f06c9639
Replit user <>
2023-07-03 13:58:23 +0000

    fix

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/whoami ', function (req, res) {
+app.get('/api/whoami', (req, res) => {
   res.json({
     ipaddress: req.socket.remoteAddress,
-    language: req.header['accept-language'],
+    language: req.headers['accept-language'],
     software: req.headers['user-agent']
   });
 });
